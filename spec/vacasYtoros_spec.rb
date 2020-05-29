dd2a1bc351bc44b468576fcea63a141366aa60d5
jotas1000000@hotmail.com <jotas1000000@hotmail.com>
2020-05-29 18:09:59 -0400

    TDD-Prueba12-Refactorizacion

--- spec/vacasYtoros_spec.rb
+++ spec/vacasYtoros_spec.rb
@@ -1,51 +1,41 @@
 require 'VacasYToros.rb'
 
 RSpec.describe VacasYToros do
-
+    before { @vacasYtoros = VacasYToros.new() }
     it 'devuelve' do
-        vacasYtoros = VacasYToros.new()
-        expect(vacasYtoros.run('1234','1234')).to eq '4 toros y 0 vacas'
+        
+        expect(@vacasYtoros.run('1234','1234')).to eq '4 toros y 0 vacas'
     end
     it 'devuelve' do
-        vacasYtoros = VacasYToros.new()
-        expect(vacasYtoros.run('1934','1234')).to eq '3 toros y 0 vacas'
+        expect(@vacasYtoros.run('1934','1234')).to eq '3 toros y 0 vacas'
     end
     it 'devuelve' do
-        vacasYtoros = VacasYToros.new()
-        expect(vacasYtoros.run('4321','1234')).to eq '0 toros y 4 vacas'
+        expect(@vacasYtoros.run('4321','1234')).to eq '0 toros y 4 vacas'
     end
     it 'devuelve' do
-        vacasYtoros = VacasYToros.new()
-        expect(vacasYtoros.run('1892','1389')).to eq '1 toros y 2 vacas'
+        expect(@vacasYtoros.run('1892','1389')).to eq '1 toros y 2 vacas'
     end
     it 'devuelve' do
-        vacasYtoros = VacasYToros.new()
-        expect(vacasYtoros.run('1293','2391')).to eq '1 toros y 3 vacas'
+        expect(@vacasYtoros.run('1293','2391')).to eq '1 toros y 3 vacas'
     end
     it 'devuelve' do
-        vacasYtoros = VacasYToros.new()
-        expect(vacasYtoros.run('12293','2391')).to eq 'Los numeros no son del mismo tamano'
+        expect(@vacasYtoros.run('12293','2391')).to eq 'Los numeros no son del mismo tamano'
     end
     it 'devuelve' do
-        vacasYtoros = VacasYToros.new()
-        expect(vacasYtoros.run('1','2391')).to eq 'Los numeros no son del mismo tamano'
+        expect(@vacasYtoros.run('1','2391')).to eq 'Los numeros no son del mismo tamano'
     end
     it 'devuelve' do
-        vacasYtoros = VacasYToros.new()
-        expect(vacasYtoros.run('abcd','2391')).to eq 'El numero no puede tener letras'
+        expect(@vacasYtoros.run('abcd','2391')).to eq 'El numero no puede tener letras'
     end
     it 'devuelve' do
-        vacasYtoros = VacasYToros.new()
-        expect(vacasYtoros.run('a1cd','2391')).to eq 'El numero no puede tener letras'
+        expect(@vacasYtoros.run('a1cd','2391')).to eq 'El numero no puede tener letras'
     end
     it 'devuelve' do
-        vacasYtoros = VacasYToros.new()
-        expect(vacasYtoros.run('a2cd391','2391')).to eq 'El numero no puede tener letras'
+        expect(@vacasYtoros.run('a2cd391','2391')).to eq 'El numero no puede tener letras'
     end
     it 'devuelve' do
-        vacasYtoros = VacasYToros.new()
-        vacasYtoros.run('zzzzzz','2391');
-        expect(vacasYtoros.run('a2cd391','2391')).to eq 'El numero no puede tener letras'
+        @vacasYtoros.run('zzzzzz','2391');
+        expect(@vacasYtoros.run('a2cd391','2391')).to eq 'El numero no puede tener letras'
     end
 end
 
