19c24e3c3bd627cf5abb3ebf52b6c59c97243cda
jotas1000000@hotmail.com <jotas1000000@hotmail.com>
2020-05-29 16:39:51 -0400

    TDD-Prueba3-Solo vacas

--- lib/vacasYtoros.rb
+++ lib/vacasYtoros.rb
@@ -1,11 +1,15 @@
 class VacasYToros 
     def play(num,secretNum) 
         toros = 0
+        vacas = 0
         for i in 0...secretNum.size do
             if ( num[i] == secretNum[i] )
                 toros += 1
-            end
+            elsif ( secretNum.include?(num[i].to_s))
+                vacas += 1
+            end 
         end
-        return "#{toros} toros y 0 vacas"
+        
+        return "#{toros} toros y #{vacas} vacas"
     end
 end
--- spec/vacasYtoros_spec.rb
+++ spec/vacasYtoros_spec.rb
@@ -10,4 +10,8 @@ RSpec.describe VacasYToros do
         vacasYtoros = VacasYToros.new()
         expect(vacasYtoros.play('1934','1234')).to eq '3 toros y 0 vacas'
     end
+    it 'devuelve' do
+        vacasYtoros = VacasYToros.new()
+        expect(vacasYtoros.play('4321','1234')).to eq '0 toros y 4 vacas'
+    end
 end
