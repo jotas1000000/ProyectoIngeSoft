fc4ade5ad1e28a33ef5350109b32a5f624d13859
jotas1000000@hotmail.com <jotas1000000@hotmail.com>
2020-05-31 22:07:40 -0400

    Dinamicidad

--- app.rb
+++ app.rb
@@ -1,4 +1,14 @@
 require 'sinatra'
+require './lib/vacasYtoros.rb'
+
+def initialize()
+    @to = 10
+    @by = 10
+end
+@@from = 10
+@@vacasYToros = VacasYToros.new()
+@@secetNumber = '1234'
+
 
 get '/' do
     erb :instrucciones
@@ -9,6 +19,19 @@ get '/Homepage' do
 end
 
 get '/Play' do
-    erb :interfazJuego
+    @@from = 10
     erb :play
+end
+
+get '/EnterNumber' do
+    @maxlength = @@secetNumber.size.to_s
+    erb :interfazJuego
+end
+
+post '/Number' do
+    @number = params['number']
+    @numberTry = @@from
+    @result = @@vacasYToros.run(@number, @@secetNumber);
+    @@from -= 1
+    erb :response
 end
