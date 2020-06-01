a03a36071a0bfc611072d965988a2aa5c17181f2
jotas1000000@hotmail.com <jotas1000000@hotmail.com>
2020-06-01 12:44:48 -0400

    Refactorizando App.rb

--- app.rb
+++ app.rb
@@ -1,13 +1,8 @@
 require 'sinatra'
 require './lib/vacasYtoros.rb'
+require './lib/GlobalVariables.rb'
 
-def initialize()
-    @to = 10
-    @by = 10
-end
-@@from = 10
-@@vacasYToros = VacasYToros.new()
-@@secetNumber = '1234'
+@@globalVariables = GlobalVariables.new(10,"12345",VacasYToros.new())
 
 
 get '/' do
@@ -19,19 +14,18 @@ get '/Homepage' do
 end
 
 get '/Play' do
-    @@from = 10
     erb :play
 end
 
 get '/EnterNumber' do
-    @maxlength = @@secetNumber.size.to_s
+    @maxlength = @@globalVariables.getSecretNumber().size.to_s
     erb :interfazJuego
 end
 
 post '/Number' do
     @number = params['number']
-    @numberTry = @@from
-    @result = @@vacasYToros.run(@number, @@secetNumber);
-    @@from -= 1
+    @numberTry = @@globalVariables.getCounterTries()
+    @result = @@globalVariables.runGame(@number)
+    @@globalVariables.discountCounterTries()
     erb :response
 end
