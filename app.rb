3fd5e93f526d41a643ff329e771ed480c94a00ce
jotas1000000@hotmail.com <jotas1000000@hotmail.com>
2020-06-03 17:17:52 -0400

    Anadiendo vista de Fin del Juego

--- app.rb
+++ app.rb
@@ -2,7 +2,7 @@ require 'sinatra'
 require './lib/vacasYtoros.rb'
 require './lib/GlobalVariables.rb'
 
-@@globalVariables = GlobalVariables.new(10,"12345",VacasYToros.new())
+@@globalVariables = GlobalVariables.new(2,"1234",VacasYToros.new())
 
 
 get '/' do
@@ -10,6 +10,7 @@ get '/' do
 end
 
 get '/Homepage' do
+    @@globalVariables.setCounterTries(2);
     erb :Homepage
 end
 
@@ -19,13 +20,28 @@ end
 
 get '/EnterNumber' do
     @maxlength = @@globalVariables.getSecretNumber().size.to_s
-    erb :interfazJuego
+    @numberTry = @@globalVariables.getCounterTries()
+    if (@numberTry == 0)
+        @messageResult = "Perdiste ! :("
+        erb :endGame
+    else 
+        erb :interfazJuego
+    end
 end
 
 post '/Number' do
     @number = params['number']
+    @messageResult = '';
     @numberTry = @@globalVariables.getCounterTries()
+    @triesRemaining = @@globalVariables.discountCounterTries()
     @result = @@globalVariables.runGame(@number)
-    @@globalVariables.discountCounterTries()
-    erb :response
+    if (@result == '4 toros y 0 vacas')
+        @messageResult = "Ganaste ! :)"
+        erb :endGame
+    elsif (@numberTry == 0)
+        @messageResult = "Perdiste ! :("
+        erb :endGame
+    elsif (@result != '4 toros y 0 vacas')        
+        erb :response
+    end
 end
